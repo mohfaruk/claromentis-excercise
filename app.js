4e26b8b7d12aaa562a705802f9a9ff8d479a8f23
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2022-12-11 14:48:45 +0000

    badges displayed

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const dropDownBtn = document.querySelectorAll(".drop-down-btn");
 
+function myFunc() {
+  console.log("it works");
+}
+
+for (const btn of dropDownBtn) {
+  btn.addEventListener("click", myFunc);
+}
