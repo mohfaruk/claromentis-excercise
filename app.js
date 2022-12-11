416157bad4ce1ac62b3df646c7285764225b917b
Mohamad Faruk <mohamadfaruk.uk~gmail.com>
2022-12-11 19:22:12 +0000

    javascript added

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const dropDownBtn = document.querySelectorAll(".drop-down-btn");
+const dropDownMenu = document.querySelector(".dropdown-content");
 
-function myFunc() {
-  console.log("it works");
+function showDropDown() {
+  dropDownMenu.classList.toggle("show");
+}
+
+function removeDropDown(event) {
+  if (!event.target.matches(dropDownBtn)) {
+    dropDownMenu.classList.remove("show");
+  }
 }
 
 for (const btn of dropDownBtn) {
-  btn.addEventListener("click", myFunc);
+  btn.addEventListener("click", showDropDown);
 }
+
+// Close the dropdown if the user clicks outside of it
+document.addEventListener("click", removeDropDown);
+
+// window.onclick = function (event) {
+//   if (!event.target.matches(dropDownBtn)) {
+//     for (let i = 0; i < dropDownMenu.clientHeight; i++) {
+//       if (dropDownMenu[i].classList.contains("show")) {
+//         dropDownMenu[i].classList.remove("show");
+//       }
+//     }
+//   }
+// };
